1c824efbffbee6b06ee69a183587cd74704b000c
Mateusz Poprawa <mateusz_poprawa@outlook.com>
2026-06-06 03:13:30 +0200

    add backend

--- infrastructure/terraform/cosmos.tf
+++ infrastructure/terraform/cosmos.tf
@@ -1,8 +1,8 @@
 resource "azurerm_cosmosdb_account" "cosmos" {
   name                = "cosmos-news-trends"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
   offer_type = "Standard"
   kind       = "GlobalDocumentDB"
 
@@ -11,20 +11,20 @@ resource "azurerm_cosmosdb_account" "cosmos" {
   }
 
   geo_location {
-    location          = data.azurerm_resource_group.rg.location
+    location          = azurerm_resource_group.rg.location
     failover_priority = 0
   }
 }
 
 resource "azurerm_cosmosdb_sql_database" "db" {
   name                = "trenddb"
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.cosmos.name
 }
 
 resource "azurerm_cosmosdb_sql_container" "trends" {
   name                = "trends"
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.cosmos.name
   database_name       = azurerm_cosmosdb_sql_database.db.name
 
