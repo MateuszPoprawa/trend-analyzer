8fc46fbe836f086a74ab0e4a28d243c7d1a7acd2
Mateusz Poprawa <mateusz_poprawa@outlook.com>
2026-06-11 16:14:12 +0200

    fix

--- infrastructure/terraform/functions.tf
+++ infrastructure/terraform/functions.tf
@@ -82,7 +82,7 @@ resource "azurerm_linux_function_app" "nlp" {
 }
 
 resource "azurerm_linux_function_app" "summary" {
-  name                = "func-summary-generator-summaries"
+  name                = "func-summaries-summary-generator"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
